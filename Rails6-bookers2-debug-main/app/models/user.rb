069816f095682a7cf3aff55c64326126d17a4204
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-08-10 06:58:52 +0000

    [Finish] oyokadai8a

--- Rails6-bookers2-debug-main/app/models/user.rb
+++ Rails6-bookers2-debug-main/app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
 
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
